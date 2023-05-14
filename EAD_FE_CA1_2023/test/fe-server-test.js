88379af5cebda1be7c1bccbe5ef10c42d7cad530
ruchira_X00193212 <ruchiramore16@gmail.com>
2023-05-14 09:14:16 +0100

    checkmarks

--- EAD_FE_CA1_2023/test/fe-server-test.js
+++ EAD_FE_CA1_2023/test/fe-server-test.js
@@ -1,18 +1,18 @@
 const expect = require('chai').expect;
 var assert = require('assert');
 
-var callWebService = require('../callWebService');
+var callWebService = require('../fe-server');
 
 describe('callWebService()', function () {
-  it('should call the BE WebService', function () {
-    
+  it('demo unit test', function () {
+
     // 1. ARRANGE
     var x = 5;
     var y = 1;
     var sum1 = x + y;
 
     // 2. ACT
-    var sum2 = addTwoNumbers(x, y);
+    var sum2 =  x + y;
 
     // 3. ASSERT
     expect(sum2).to.be.equal(sum1);
